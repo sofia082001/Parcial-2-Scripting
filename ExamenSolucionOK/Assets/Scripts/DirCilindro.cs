abe5ef12b0d297d7fa2247a9b5d3f8b04040ace9
Sebastian Campuzano <sebaska2025@gmail.com>
2021-09-21 21:46:30 -0500

    Proyecto Base

---
+++ ExamenSolucionOK/Assets/Scripts/DirCilindro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirCilindro : MonoBehaviour
+{
+    public Vector3 dirCilindro = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Cilindro obj = other.GetComponent<Cilindro>();
+        obj.dirVector3 = dirCilindro;
+
+    }
+}
