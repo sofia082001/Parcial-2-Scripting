6adae78b77fd6efb625224375e8810f3d015b189
Sebastian Campuzano <sebaska2025@gmail.com>
2021-09-23 15:02:51 -0500

    Proyecto Base v2

---
+++ Clase Agosto 20/Assets/scripts/bottonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bottonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text Marcador;
+    private int marcadornumero = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void AumentarMarcador()
+    {
+        marcadornumero++;
+        Marcador.text = marcadornumero.ToString();
+    }
+
+    
+}
