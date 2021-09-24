478d07d87d4a27d58d2d5cf045892ea3e51560c7
Sofia <sofia.oyola@upb.edu.co>
2021-09-24 15:50:36 -0500

    Auto stash before merge of "main" and "origin/main"

--- Clase Agosto 20/Assets/scripts/eventControllerSO.cs
+++ Clase Agosto 20/Assets/scripts/eventControllerSO.cs
@@ -10,7 +10,27 @@ public class eventControllerSO : ScriptableObject
     public delegate void CambieColor();
     public event CambieColor cambieColor;
 
+<<<<<<< Updated upstream
     public void Cambiar_color()
+=======
+    public delegate void CambieColorAzul();
+    public event CambieColorAzul cambieColorAzul;
+
+    public delegate void CambieColorRojo();
+    public event CambieColorRojo cambieColorRojo;
+
+    public delegate void CambieColor();
+    public event CambieColor cambieColor; 
+
+    //public event UnityAction cambiaColor;
+    //public event UnityAction<float> cambiaTamano;
+
+    public void CambiarColorEspheraAzul()
+    {
+        cambieColorAzul?.Invoke();
+    }
+    public void CambiarColorEspheraRojo()
+>>>>>>> Stashed changes
     {
         cambieColor?.Invoke();
     }
