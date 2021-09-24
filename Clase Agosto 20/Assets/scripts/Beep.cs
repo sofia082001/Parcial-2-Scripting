803546cfb553d9efc0ec0b4ddef25951f0d19422
Diego F <diegoloza12@hotmail.com>
2021-09-24 16:03:36 -0500

    Implementé el principio O

---
+++ Clase Agosto 20/Assets/scripts/Beep.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beep : MonoBehaviour
+{
+    [SerializeField] private eventControllerSO canal;
+
+    [SerializeField] private AudioClip clip;
+
+    private AudioSource player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<AudioSource>();
+        player.clip = clip;
+    }
+
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+        canal.cambieColor += PlayBeep;
+    }
+    private void OnDisable()
+    {
+        canal.cambieColor -= PlayBeep;
+    }
+    public void PlayBeep()
+    {
+        player.Play();
+    }
+}
