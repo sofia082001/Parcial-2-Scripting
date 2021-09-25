bd1e439813c5eaca3cb48655a6000e211d46f363
Diego F <diegoloza12@hotmail.com>
2021-09-25 14:10:34 -0500

    Terminé el patrón State

--- Clase Agosto 20/Assets/scripts/ToggleUpdater.cs
+++ Clase Agosto 20/Assets/scripts/ToggleUpdater.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleUpdater : MonoBehaviour
 {   
     [SerializeField] eventControllerSO eventoToggle;
+    [SerializeField] GameObject esfera;
+    ToggleController togglEstado = new ToggleController();
+    public Text textoprincipal;
 
     private void Start()
     {
@@ -21,5 +25,13 @@ public class ToggleUpdater : MonoBehaviour
     public void toggleUpdateState()
     {
         Debug.Log("entre en estado Toggle");
+        if(esfera.gameObject.activeSelf)
+        {
+            togglEstado.Activado(textoprincipal);
+        }
+        if (!esfera.gameObject.activeSelf)
+        {
+            togglEstado.Apagado(textoprincipal);
+        }
     }
 }
